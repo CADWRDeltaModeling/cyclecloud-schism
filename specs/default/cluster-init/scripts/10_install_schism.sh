873f70a0a972e7e7f415653ec6b1ac98adb37c1a
Kijin Nam <knam@water.ca.gov>
2019-03-08 21:34:02 +0000

    Add gcc installation and an example

--- specs/default/cluster-init/scripts/10_install_schism.sh
+++ specs/default/cluster-init/scripts/10_install_schism.sh
@@ -17,7 +17,7 @@ export HDF5_DIR=/opt/hdf5/1.10.5/intel
 export NETCDF_C_DIR=/opt/netcdf-c/4.6.2/intel
 export NETCDF_FORTRAN_DIR=/opt/netcdf-fortran/4.4.4/intel
 export LD_LIBRARY_PATH=$NETCDF_FORTRAN_DIR/lib:$NETCDF_C_DIR/lib:$HDF5_DIR/lib:$SZIP_DIR/lib:$LD_LIBRARY_PATH
-export PATH=/opt/schism/truk:$PATH
+export PATH=/opt/schism/trunk:$PATH
 EOF
 chmod 755 /etc/profile.d/schism.sh
 
