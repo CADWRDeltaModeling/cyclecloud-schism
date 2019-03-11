8ea528715f9129a52c8cf25d7c0644bf22ae7739
Kijin Nam <knam@water.ca.gov>
2019-03-11 17:44:00 +0000

    Add idle times and fix bugs in sh

--- specs/default/cluster-init/scripts/00_install_intel_runtime.sh
+++ specs/default/cluster-init/scripts/00_install_intel_runtime.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 set -x
+yum install -y bash-completion
 yum install -y gcc
 rpm --import https://yum.repos.intel.com/2019/setup/RPM-GPG-KEY-intel-psxe-runtime-2019
 rpm -Uhv https://yum.repos.intel.com/2019/setup/intel-psxe-runtime-2019-reposetup-1-0.noarch.rpm
--- specs/default/cluster-init/scripts/10_install_schism.sh
+++ specs/default/cluster-init/scripts/10_install_schism.sh
@@ -12,12 +12,13 @@ tar xjf /tmp/$SCHISM_TAR
 
 cat <<EOF > /etc/profile.d/schism.sh
 #!/bin/bash
+source /opt/intel/psxe_runtime/linux/bin/psxevars.sh intel64
 export SZIP_DIR=/opt/szip/2.1.1/intel
 export HDF5_DIR=/opt/hdf5/1.10.5/intel
 export NETCDF_C_DIR=/opt/netcdf-c/4.6.2/intel
 export NETCDF_FORTRAN_DIR=/opt/netcdf-fortran/4.4.4/intel
-export LD_LIBRARY_PATH=$NETCDF_FORTRAN_DIR/lib:$NETCDF_C_DIR/lib:$HDF5_DIR/lib:$SZIP_DIR/lib:$LD_LIBRARY_PATH
-export PATH=/opt/schism/trunk:$PATH
+export LD_LIBRARY_PATH=\$NETCDF_FORTRAN_DIR/lib:\$NETCDF_C_DIR/lib:\$HDF5_DIR/lib:\$SZIP_DIR/lib:\$LD_LIBRARY_PATH
+export PATH=/opt/schism/trunk:\$PATH
 EOF
 chmod 755 /etc/profile.d/schism.sh
 
