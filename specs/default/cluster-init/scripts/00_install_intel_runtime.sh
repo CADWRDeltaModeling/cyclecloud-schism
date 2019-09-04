53c3c7ed7876003e54536533f806dfd7064d56ac
Kijin Nam <knam@water.ca.gov>
2019-09-04 18:50:25 +0000

    Add an image and NFS size selection

--- specs/default/cluster-init/scripts/00_install_intel_runtime.sh
+++ specs/default/cluster-init/scripts/00_install_intel_runtime.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-set -e
-set -x
-
-yum install -y gcc
-
-INTEL_VER=2016.3-210
-# The ordering of the files follows dependencies.
-files="intel-psxe-common-runtime-${INTEL_VER}.noarch.rpm intel-comp-common-runtime-${INTEL_VER}.noarch.rpm intel-comp-runtime-64-${INTEL_VER}.x86_64.rpm intel-openmp-runtime-64-16.0.3-210.x86_64.rpm intel-mpi-runtime-64-${INTEL_VER}.x86_64.rpm intel-ifort-common-runtime-${INTEL_VER}.noarch.rpm intel-ifort-runtime-64-${INTEL_VER}.x86_64.rpm intel-icc-common-runtime-${INTEL_VER}.noarch.rpm intel-icc-runtime-64-${INTEL_VER}.x86_64.rpm"
-cd /tmp
-for f in $files; do
-  jetpack download $f /tmp/
-  rpm -i $f
-done
+# set -e
+# set -x
+# 
+# yum install -y gcc
+# 
+# INTEL_VER=2016.3-210
+# # The ordering of the files follows dependencies.
+# files="intel-psxe-common-runtime-${INTEL_VER}.noarch.rpm intel-comp-common-runtime-${INTEL_VER}.noarch.rpm intel-comp-runtime-64-${INTEL_VER}.x86_64.rpm intel-openmp-runtime-64-16.0.3-210.x86_64.rpm intel-mpi-runtime-64-${INTEL_VER}.x86_64.rpm intel-ifort-common-runtime-${INTEL_VER}.noarch.rpm intel-ifort-runtime-64-${INTEL_VER}.x86_64.rpm intel-icc-common-runtime-${INTEL_VER}.noarch.rpm intel-icc-runtime-64-${INTEL_VER}.x86_64.rpm"
+# cd /tmp
+# for f in $files; do
+#   jetpack download $f /tmp/
+#   rpm -i $f
+# done
 
--- specs/default/cluster-init/scripts/10_install_schism.sh
+++ specs/default/cluster-init/scripts/10_install_schism.sh
@@ -3,19 +3,19 @@
 set -x
 set -e
 
-yum install -y netcdf-fortran
-
-SCHISM_TAR=schism.0.9.2.tar.gz
-
-jetpack download $SCHISM_TAR /tmp/
-
-cd /opt
-tar xzf /tmp/$SCHISM_TAR
-
-cat <<EOF > /etc/profile.d/schism.sh
-#!/bin/bash
-source /opt/intel/psxe_runtime/linux/bin/psxevars.sh intel64
-export PATH=/opt/schism/trunk_intel2016_mpi_only:\$PATH
-EOF
-chmod 755 /etc/profile.d/schism.sh
+# yum install -y netcdf-fortran
+# 
+# SCHISM_TAR=schism.0.9.2.tar.gz
+# 
+# jetpack download $SCHISM_TAR /tmp/
+# 
+# cd /opt
+# tar xzf /tmp/$SCHISM_TAR
+# 
+# cat <<EOF > /etc/profile.d/schism.sh
+# #!/bin/bash
+# source /opt/intel/psxe_runtime/linux/bin/psxevars.sh intel64
+# export PATH=/opt/schism/trunk_intel2016_mpi_only:\$PATH
+# EOF
+# chmod 755 /etc/profile.d/schism.sh
 
